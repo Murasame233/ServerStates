3cf5d80bebf1082def533909e5200e3baafa263b
Murasame <me@murasame.moe>
2021-09-16 07:27:29 +0800

    It runs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use cc;
 fn main() {
-    cc::Build::new().file("c/macos.h")
+    cc::Build::new().file("c/macos.h").warnings(false)
     .compile("libgetinfo");
     println!("cargo:rerun-if-changed=c/macos.h")
 }
