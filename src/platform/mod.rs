75095949be7c4dde6afb53070703792431ea3c6c
Murasame <me@murasame.moe>
2021-09-22 12:21:18 +0000

    FMT: format

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -8,18 +8,18 @@ pub mod linux;
 pub use linux::Linux;
 
 pub trait Platform {
-    fn init()->Self;
-    fn get_all(&self)->&'_ [i32];
+    fn init() -> Self;
+    fn get_all(&self) -> &'_ [i32];
     fn update(&mut self);
-    fn get(&self,core:usize)->i32;
+    fn get(&self, core: usize) -> i32;
 }
 
 #[cfg(target_os = "macos")]
-pub fn get_platform()-> impl Platform {
+pub fn get_platform() -> impl Platform {
     MacOS::init()
 }
 
 #[cfg(target_os = "linux")]
-pub fn get_platform()-> impl Platform {
+pub fn get_platform() -> impl Platform {
     Linux::init()
 }
