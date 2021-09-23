fcfdd0cd20b7f79d0a0733a1a6e01fa8153896fd
Murasame <me@murasame.moe>
2021-09-23 02:31:49 +0000

    save my work

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -1,23 +1,87 @@
-use super::Platform;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::{Path, PathBuf},
+};
 
-pub struct Linux{
+use super::{Count, CountsAll, Platform};
 
+pub struct Linux {
+    path: PathBuf,
+    prev_counts: Option<CountsAll>,
+    cores: usize,
+    core_status: Vec<i32>,
+    all_status:u8
+}
+
+impl Linux {
+    fn read_file(&self) -> CountsAll {
+        let mut count: CountsAll = CountsAll {
+            cores: Vec::new(),
+            all: Count { total: 0, used: 0 },
+        };
+        let file = File::open(&self.path).unwrap();
+        let mut reader = BufReader::new(file);
+        loop {
+            let mut string = String::new();
+            reader.read_line(&mut string).unwrap();
+
+            // A single line format: cpu{core} user nice system idle
+            let mut s = string.split_ascii_whitespace();
+            if let Some(v) = s.next() {
+                if v.starts_with("cpu") {
+                    let (_, numstr) = v.split_at(3);
+                    let used = s.next().unwrap().parse::<u64>().unwrap()
+                        + s.next().unwrap().parse::<u64>().unwrap()
+                        + s.next().unwrap().parse::<u64>().unwrap();
+                    let total = count.all.used + s.next().unwrap().parse::<u64>().unwrap();
+                    match numstr.parse::<usize>() {
+                        // Have number after 'cpu' means core.
+                        Ok(_) => count.cores.push(Count { used, total }),
+                        // No number after 'cpu' means all core.
+                        Err(_) => {
+                            count.all.used = used;
+                            count.all.total = total
+                        }
+                    };
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        count
+    }
 }
 
 impl Platform for Linux {
-    fn init()->Self {
-        todo!()
+    fn init() -> Self {
+        let path = Path::new("/proc/stat").to_owned();
+        let mut l = Linux {
+            path,
+            prev_counts: None,
+            cores: 0,
+            core_status: Vec::new(),
+            all_status:0
+        };
+        l.prev_counts = Some(l.read_file());
+        l.cores = l.prev_counts.as_ref().unwrap().cores.len();
+        l
     }
 
-    fn get_all(&self)->&'_ [i32] {
-        todo!()
+    fn get_all(&self) -> &'_ [i32] {
+        self.core_status.as_ref()
     }
 
     fn update(&mut self) {
-        todo!()
+        let newcount = self.read_file();
+        let oldcount = self.prev_counts.as_mut().unwrap();
+        self.all_status = ((newcount.all.used - oldcount.all.used)/(newcount.all.total - oldcount.all.total) * 100u64) as u8;
+        *oldcount = newcount;
     }
 
-    fn get(&self,core:usize)->i32 {
-        todo!()
+    fn get(&self, core: usize) -> i32 {
+        self.core_status[core-1]
     }
 }
