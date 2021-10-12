5a0095a974b7d6e0df06b057e9596650ba5f61da
Murasame <me@murasame.moe>
2021-10-12 08:18:36 +0000

    Update files

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -11,7 +11,7 @@ pub struct Linux {
     prev_counts: Option<CountsAll>,
     cores: usize,
     core_status: Vec<i32>,
-    all_status:u8
+    all_status: u8,
 }
 
 impl Linux {
@@ -63,10 +63,11 @@ impl Platform for Linux {
             prev_counts: None,
             cores: 0,
             core_status: Vec::new(),
-            all_status:0
+            all_status: 0,
         };
         l.prev_counts = Some(l.read_file());
         l.cores = l.prev_counts.as_ref().unwrap().cores.len();
+        l.core_status.resize(l.cores, 0);
         l
     }
 
@@ -77,11 +78,19 @@ impl Platform for Linux {
     fn update(&mut self) {
         let newcount = self.read_file();
         let oldcount = self.prev_counts.as_mut().unwrap();
-        self.all_status = ((newcount.all.used - oldcount.all.used)/(newcount.all.total - oldcount.all.total) * 100u64) as u8;
+        self.all_status = ((newcount.all.used - oldcount.all.used) as f32
+            / (newcount.all.total - oldcount.all.total) as f32
+            * 100f32) as u8;
+        for (index, value) in (&mut self.core_status).iter_mut().enumerate() {
+            let new = &newcount.cores[index];
+            let old = &oldcount.cores[index];
+            *value =
+                ((new.used - old.used) as f32 / (new.total - old.total) as f32 * 100f32) as i32;
+        }
         *oldcount = newcount;
     }
 
     fn get(&self, core: usize) -> i32 {
-        self.core_status[core-1]
+        self.core_status[core - 1]
     }
 }
